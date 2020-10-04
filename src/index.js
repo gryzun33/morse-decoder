06f8d07a45da4ef278e237073ea126e2cfcd7514
gryzun33 <yakushevao777@gmail.com>
2020-10-04 09:00:16 +0300

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -35,11 +35,43 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let currLetterMorse;
+    let currCode;
+    let currSymbol;
+    let arrExpr = expr.split('');
+  
+    for (let i = 0; i < arrExpr.length; i = i+10) {
+        currLetterMorse = ''; 
+        currCode = arrExpr.slice( i, i + 10);
+        console.log (currCode);
+        if (currCode.join('') === '**********' ) {
+            result = result + ' ';
+        } else {
+            for (j = 0; j < currCode.length; j = j+2) {
+                currSymbol = '' + currCode[j] + currCode[j+1];
+                console.log(currSymbol);
+                if (currSymbol === '10') {
+                    currLetterMorse = currLetterMorse + '.';
+                }
+                if (currSymbol === '11') {
+                    currLetterMorse = currLetterMorse + '-';
+                }
+              console.log(currLetterMorse);
+            }
+
+            result = result + MORSE_TABLE[currLetterMorse];
+
+        }
+    }
+ 
+    return result;
 }
+
 
 module.exports = {
     decode
